1b96bd7045f5272b04e0f767e29a17f984ce17fe
Kaan Kani İnce <kkince@newmind.ai>
2024-07-05 17:06:37 +0300

    Load-more-button created

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoadMoreData from "./components/load-more-data"
+
+function App() {
+  return (
+    <div className="App">
+      {/* Load more products*/}
+      <LoadMoreData/>
+    </div>
+  );
+}
+
+export default App;
